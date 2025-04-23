0aed567f6b2d944e48fb7f6f6dae4c97eb8f6ad7
TerrazasJr316 <terrazasjosue0@gmail.com>
2025-04-23 13:25:29 -0600

    API config

--- main.py
+++ main.py
@@ -155,11 +155,10 @@ def caracteristicas():
         "paquetes": paquetes
     })
 
-""""
 if __name__ == '__main__':
     app.run(debug=True)
-"""
 
-""""""
+""""
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
+"""
